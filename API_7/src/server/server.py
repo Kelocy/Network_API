219966cabe29c832bb93aa2045f2bf643aea24ab
= <=>
2023-05-10 09:18:27 +0900

    test

--- API_7/src/server/server.py
+++ API_7/src/server/server.py
@@ -1,14 +1,13 @@
+"""Server side by using ssl to receive and send message 
+"""
 import socket
 import ssl
 
-# context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-# context.load_cert_chain(certfile='server_cert.pem', keyfile='server_key.pem')
-
 
 class Server:
-    def __init__(self, HOST, PORT, cert, key):
-        self.HOST = HOST
-        self.PORT = PORT
+    def __init__(self, host, port, cert, key):
+        self.host = host
+        self.port = port
         self.cert = cert
         self.key = key
 
@@ -17,16 +16,17 @@ class Server:
         context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         context.load_cert_chain(certfile=self.cert, keyfile=self.key)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.bind((self.HOST, self.PORT))
+            sock.bind((self.host, self.port))
             sock.listen(5)
             with context.wrap_socket(sock, server_side=True) as ssock:
                 while True:
-                    conn, addr = ssock.accept()
+                    conn, = ssock.accept()
                     try:
                         data = conn.recv(1024)
                         if not data:
                             break
                         print("Receive data: ", data.decode())
                         conn.sendall(data)
-                    except:
-                        print("Connection Error")
+                    except ssl.SSLError as SSLError:
+                        raise ConnectionError(
+                            "SSL Connection Error") from SSLError
