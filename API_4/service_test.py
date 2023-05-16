dbea27c4dc82aa8cd1d3f6cd81b96c62d7302d34
= <=>
2023-05-16 09:36:59 +0900

    fix path error while opening the file in API_4 and API_7

--- API_4/service_test.py
+++ API_4/service_test.py
@@ -1,19 +1,24 @@
 import unittest
 from src.IPGenerator import IPGenerator
+import os
 
 
 class ServiceTest(unittest.TestCase):
     def test_generate_ipv4(self):
         ipv4 = "123.45.67.89/27"
         Address_ipv4 = IPGenerator(cidr_network=ipv4)
-        data = file_to_list("ipv4.txt")
+        file_path = os.path.join(os.path.dirname(__file__), 'ipv4.txt')
+
+        data = file_to_list(file_path)
         self.assertEqual(data, Address_ipv4.ip_list())
         # ipv4_list = Address_ipv4.ip_list()
 
     def test_generate_ipv6(self):
         ipv6 = "2001:db8::/120"
         Address_ipv6 = IPGenerator(cidr_network=ipv6)
-        data = file_to_list("ipv6.txt")
+        file_path = os.path.join(os.path.dirname(__file__), 'ipv6.txt')
+
+        data = file_to_list(file_path)
         self.assertEqual(data, Address_ipv6.ip_list())
         # ipv6_list = Address_ipv6.ip_list()
 
--- API_7/service_test.py
+++ API_7/service_test.py
@@ -5,19 +5,24 @@ import threading
 from src.server.server import Server
 from src.client.client import Client
 import time
+import os
 
 
 class ServiceTest(unittest.TestCase):
     def test_run(self):
         HOST, PORT = "localhost", 9999
-        cert_server, key_server = 'src/server/cert.pem', 'src/server/key.pem'
+        cert_server = os.path.join(
+            os.path.dirname(__file__), 'src/server/cert.pem')
+        key_server = os.path.join(
+            os.path.dirname(__file__), 'src/server/key.pem')
         server = Server(HOST, PORT, cert_server, key_server)
         server_thread = threading.Thread(target=server.start)
         server_thread.start()
 
         time.sleep(1)
 
-        cert_client = 'src/client/cert.pem'
+        cert_client = os.path.join(
+            os.path.dirname(__file__), 'src/client/cert.pem')
         client = Client(HOST, PORT, cert_client)
         data = "Hello World!"
 
